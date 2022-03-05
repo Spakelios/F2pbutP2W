73c3ac90a95a172d524404398a3dc9a15ba197a6
Spakelios <73300254+Spakelios@users.noreply.github.com>
2022-03-05 21:22:27 +0000

    boop

---
+++ BigTiddySimulator/Assets/Kellies/Code/DialogueSystem.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueSystem : MonoBehaviour 
+{
+	public static DialogueSystem instance;
+
+	public ELEMENTS elements;
+
+	void Awake()
+	{
+		instance = this;
+	}
+	
+	public void Say(string speech, string speaker = "")
+	{
+		StopSpeaking();
+
+		speaking = StartCoroutine(Speaking(speech, false, speaker));
+	}
+	
+	public void SayAdd(string speech, string speaker = "")
+	{
+		StopSpeaking();
+
+		speechText.text = targetSpeech;
+
+		speaking = StartCoroutine(Speaking(speech, true, speaker));
+	}
+
+	public void StopSpeaking()
+	{
+		if (isSpeaking)
+		{
+			StopCoroutine(speaking);
+		}
+		speaking = null;
+	}
+		
+	public bool isSpeaking {get{return speaking != null;}}
+	[HideInInspector] public bool isWaitingForUserInput = false;
+
+	public string targetSpeech = "";
+	Coroutine speaking = null;
+	IEnumerator Speaking(string speech, bool additive, string speaker = "")
+	{
+		speechPanel.SetActive(true);
+		targetSpeech = speech;
+
+		if (!additive)
+			speechText.text = "";
+		else
+			targetSpeech = speechText.text + targetSpeech;
+
+		speakerNameText.text = DetermineSpeaker(speaker);//temporary
+
+		isWaitingForUserInput = false;
+
+		while(speechText.text != targetSpeech)
+		{
+			speechText.text += targetSpeech[speechText.text.Length];
+			yield return new WaitForEndOfFrame();
+		}
+
+		//text finished
+		isWaitingForUserInput = true;
+		while(isWaitingForUserInput)
+			yield return new WaitForEndOfFrame();
+
+		StopSpeaking();
+	}
+
+	string DetermineSpeaker(string s)
+	{
+		string retVal = speakerNameText.text;//default return is the current name
+		if (s != speakerNameText.text && s != "")
+			retVal = (s.ToLower().Contains("narrator")) ? "" : s;
+
+		return retVal;
+	}
+
+	[System.Serializable]
+	public class ELEMENTS
+	{
+		/// <summary>
+		/// The main panel containing all dialogue related elements on the UI
+		/// </summary>
+		public GameObject speechPanel;
+		public TextMeshProUGUI speakerNameText;
+		public TextMeshProUGUI speechText;
+	}
+	public GameObject speechPanel {get{return elements.speechPanel;}}
+	public TextMeshProUGUI speakerNameText {get{return elements.speakerNameText;}}
+	public TextMeshProUGUI speechText {get{return elements.speechText;}}
+}
---
+++ BigTiddySimulator/Assets/Kellies/Code/Doesitwork.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Doesitwork : MonoBehaviour
+{
+ private DialogueSystem dialogue;
+ private int index = 0;
+
+ private void Start()
+ {
+  dialogue = DialogueSystem.instance;
+ }
+
+ public string[] s = new string[]
+ {
+ "nice tits:Simp",
+ "ty i grew them myself",
+ "woah based"
+ };
+
+ private void Update()
+ {
+  if (Input.GetKeyDown(KeyCode.Space))
+  {
+   if (!dialogue.isSpeaking || dialogue.isWaitingForUserInput)
+   {
+    if (index >= s.Length)
+    {
+     return;
+    }
+    Say(s[index]);
+    index++;
+   }
+  }
+ }
+
+ void Say(string s)
+ {
+  string[] parts = this.s;
+  string speech = parts[0];
+  string speaker = (parts.Length >= 2 ? parts[1] : ":");
+  
+  dialogue.Say(speech, speaker);
+ }
+}
