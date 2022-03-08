b25887b48c8c1150c3dcf44a3fd0629607bcf254
JoelleT02 <77802831+JoelleT02@users.noreply.github.com>
2022-03-08 00:06:24 +0000

    Hehe Funny Gacha Button

---
+++ BigTiddySimulator/Assets/Jos/Code/CurrencyManager.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CurrencyManager : MonoBehaviour
+{
+    public TextMeshProUGUI noteCounter;
+    public int totalNotes;
+    private GachaTable gachaTable;
+
+
+    private void Start()
+    {
+        noteCounter.text = "Music Notes: " + totalNotes;
+    }
+
+    private void Update()
+    {
+        noteCounter.text = "Music Notes: " + totalNotes; 
+    }
+}
--- BigTiddySimulator/Assets/Jos/Code/GachaTable.cs
+++ BigTiddySimulator/Assets/Jos/Code/GachaTable.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GachaTable : MonoBehaviour
 {
@@ -11,6 +13,16 @@ public class GachaTable : MonoBehaviour
 
     public int total;
     public int randomDraw;
+    public Button funnyButton;
+    public int pullPrice;
+    private CurrencyManager noteAmount;
+    private GameObject theManager;
+    public CurrencyManager currencyManager;
+    private bool didPull;
+    public GameObject commonPool;
+    public GameObject uncommonPool;
+    public GameObject rarePool;
+
 
     private void Start()
     {
@@ -19,20 +31,59 @@ public class GachaTable : MonoBehaviour
             total += instrument; //tally the total weight
         }
 
-        randomDraw = UnityEngine.Random.Range(0, total);
-        
-        for (int i = 0; i < gachaRates.Length; i++)
-        {
-            if (randomDraw <= gachaRates[i])
-            {
-                indicators[i].SetActive(true);
-                return;
-            }
+        funnyButton.onClick.AddListener(TaskOnClick);
+        theManager = GameObject.Find("CurrencyManager");
+        currencyManager = theManager.GetComponent<CurrencyManager>();
+        didPull = false;
+        commonPool.SetActive(false);
+        uncommonPool.SetActive(false);
+        rarePool.SetActive(false);
+    }
 
-            else
-            {
-                randomDraw -= gachaRates[i];
-            }
+
+    void TaskOnClick()
+    {
+
+
+        if (currencyManager.totalNotes >= pullPrice)
+        {
+            randomDraw = UnityEngine.Random.Range(0, total);
+            currencyManager.totalNotes -= pullPrice;
+
+            for (int i = 0; i < gachaRates.Length; i++)
+
+                if (randomDraw <= gachaRates[i])
+                {
+                    indicators[i].SetActive(true);
+                    didPull = true;
+                    
+                    if (commonPool.activeInHierarchy || uncommonPool.activeInHierarchy || rarePool.activeInHierarchy)
+                    {
+                        Invoke(nameof(Deactivate), 1.0f);
+                    }
+                    
+                    return;
+                    
+                    
+                }
+
+                else
+                {
+                    randomDraw -= gachaRates[i];
+                }
+        }
+
+        else
+        {
+            Debug.Log("Smells like poor in here lmao");
         }
     }
+
+    void Deactivate()
+    {
+        commonPool.SetActive(false);
+        uncommonPool.SetActive(false);
+        rarePool.SetActive(false);
+    }
 }
+
