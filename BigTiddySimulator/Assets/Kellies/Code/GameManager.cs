f4ca1e3cf72d42f08ff1557800906714e3908fad
Spakelios <73300254+Spakelios@users.noreply.github.com>
2022-03-08 20:28:51 +0000

    ,kjnbhgvfcdxazs

--- BigTiddySimulator/Assets/Kellies/Code/GameManager.cs
+++ BigTiddySimulator/Assets/Kellies/Code/GameManager.cs
@@ -8,10 +8,10 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-
+    
     public AudioSource theMusic;
 
-    public bool startPLaying = true;
+    public bool startPLaying;
 
     public BeatScroller theBS;
 
@@ -20,31 +20,19 @@ public class GameManager : MonoBehaviour
 
     public int ScorePerGoodNote = 125;
     public int ScorePerPerfectNote = 150;
-
+    
     public int currentMultiplier;
     public int multiplierTracker;
     public int[] multiplierThresholds;
 
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI multiplierText;
-
-    public float totalNotes;
-    public float normalHits;
-    public float goodHits;
-    public float perfectHits;
-    public float missedHits;
-
-    public GameObject resultsScreen;
-    public TextMeshProUGUI percentageText, goodsText, greatsText, perfectsText, missedsText;
-
+    
     private void Start()
     {
         instance = this;
 
-        scoreText.text = "Score: 0";
-        currentMultiplier = 2;
-
-        totalNotes = FindObjectsOfType<NoteArea>().Length;
+        currentMultiplier = 1;
     }
 
     private void Update()
@@ -58,80 +46,53 @@ public class GameManager : MonoBehaviour
 
                 theMusic.Play();
             }
-            else
-            {
-                if (!theMusic.isPlaying && !resultsScreen.activeInHierarchy)
-                {
-                    resultsScreen.SetActive(true);
-                   
-
-                    goodsText.text = normalHits.ToString();
-                    greatsText.text = goodHits.ToString();
-                    perfectsText.text = perfectHits.ToString();
-                    missedsText.text = missedHits.ToString();
-
-                    float totalHit = normalHits + goodHits + perfectHits;
-                    float percentHit = (totalHit / totalNotes) * 100f;
-
-                    percentageText.text = percentHit.ToString("F1") + "%";
-
-                }
-            }
         }
     }
 
-
-public void NoteHit()
+    public void NoteHit()
+    {
+        Debug.Log("Hit on Time");
+       
+        if (currentMultiplier - 1 < multiplierThresholds.Length)
         {
-            Debug.Log("Hit on Time");
+            multiplierTracker++;
 
-            if (currentMultiplier - 1 < multiplierThresholds.Length)
+            if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
             {
-                multiplierTracker++;
-
-                if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
-                {
-                    multiplierTracker = 0;
-                    currentMultiplier++;
-                }
+                multiplierTracker = 0;
+                currentMultiplier++;
             }
-
-            //currentScore += ScorePerNote * currentMultiplier;
-            scoreText.text = "Score: " + currentScore;
-            multiplierText.text = "Multiplier: " + currentMultiplier;
         }
-    
+
+        //currentScore += ScorePerNote * currentMultiplier;
+        scoreText.text = "Score: " + currentScore;
+        multiplierText.text = "Multiplier: " + currentMultiplier;
+    }
 
     public void NormalHit()
-        {
-            normalHits++;
-            currentScore += ScorePerNote;
-            NoteHit();
-
-        }
-
-        public void GoodHit()
-        {
-            goodHits++;
-            currentScore += ScorePerGoodNote;
-            NoteHit();
-        }
-
-        public void PerfectHit()
-        {
-            perfectHits++;
-            currentScore += ScorePerPerfectNote;
-            NoteHit();
-        }
-
-        public void NoteMissed()
-        {
-            Debug.Log("Missed");
-
-            missedHits++;
-
-            currentMultiplier = 1;
-            multiplierTracker = 0;
-            multiplierText.text = "Multiplier: " + currentMultiplier;
-        }
+    {
+        currentScore += ScorePerNote;
+        NoteHit();
     }
+
+    public void GoodHit()
+    {
+        currentScore += ScorePerGoodNote;
+        NoteHit();
+    }
+
+    public void PerfectHit()
+    {
+        currentScore += ScorePerPerfectNote;
+        NoteHit();
+    }
+    
+    public void NoteMissed()
+    {
+        Debug.Log("Missed");
+
+        multiplierTracker = 0;
+        currentMultiplier = 1;
+        multiplierText.text = "Multiplier: " + currentMultiplier;
+    }
+}
